d421233121d82444d243a196649af67a0d9bdd3a
Sung Jun Jin <sungjun@peoplefund.co.kr>
2022-03-07 23:39:45 +0900

    fix: 이전 hash 못가져오는 오류 수정

--- dist/index.js
+++ dist/index.js
@@ -10,26 +10,27 @@ class Block {
         this.timestamp = timestamp;
     }
 }
-Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index, previousHash, timestamp, data).toString();
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
     typeof aBlock.hash === "string" &&
     typeof aBlock.previousHash === "string" &&
     typeof aBlock.timestamp === "number" &&
     typeof aBlock.data === "string";
-const genesisBlock = new Block(0, "123lkmweflkmawef", "", "Hello", 123456);
+const genesisBlock = new Block(0, "2020202020202", "", "Hello", 123456);
 let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 const createNewBlock = (data) => {
     const previousBlock = getLatestBlock();
     const newIndex = previousBlock.index + 1;
-    const nextTimeStamp = getNewTimeStamp();
-    const nextHash = Block.calculateBlockHash(newIndex, previousBlock.hash, nextTimeStamp, data);
-    const newBlock = new Block(newIndex, nextHash, previousBlock.hash, data, nextTimeStamp);
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     addBlock(newBlock);
     return newBlock;
 };
-const getHashForBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 const isBlockValid = (candidateBlock, previousBlock) => {
     if (!Block.validateStructure(candidateBlock)) {
         return false;
@@ -40,7 +41,7 @@ const isBlockValid = (candidateBlock, previousBlock) => {
     else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
     }
-    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
         return false;
     }
     else {
